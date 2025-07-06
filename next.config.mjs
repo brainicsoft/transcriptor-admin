59e2006a92b6d38dfd219c05a2e4f466bc1228af
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-07-07 02:58:11 +0600

    asdas

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+    async headers() {
+    return [
+      {
+        source: '/uploads/:path*',
+        headers: [
+          { 
+            key: 'Cache-Control', 
+            value: 'public, max-age=31536000, immutable' 
+          }
+        ]
+      }
+    ]
+  },
   async rewrites() {
     return [
       {
