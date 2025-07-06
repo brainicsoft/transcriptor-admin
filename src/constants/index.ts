4dd81906027133a7596544db9181590a1068b7c4
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-07-07 02:23:48 +0600

    transcriptor admin added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
-export const serverUrl = (process.env.BASE_URL ? process.env.BASE_URL + '/api' : 'https://transcriptorvascular.com/api');
+// export const serverUrl = (process.env.BASE_URL ? process.env.BASE_URL + '/api' : 'https://transcriptorvascular.com/api');
+export const serverUrl = 'http://localhost:3000/api'
