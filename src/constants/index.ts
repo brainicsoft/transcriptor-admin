b7f2ac9bbbf41ee4a70cb1b3430d83bc20419199
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2025-07-22 02:15:23 +0600

    changed index url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-// export const serverUrl = (process.env.BASE_URL ? process.env.BASE_URL + '/api' : 'https://transcriptorvascular.com/api');
-export const serverUrl = 'http://localhost:3000/api'
+export const serverUrl = (process.env.BASE_URL ? process.env.BASE_URL + '/api' : 'https://transcriptorvascular.com/api');
+// export const serverUrl = 'http://localhost:3000/api'
